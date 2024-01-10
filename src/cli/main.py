634b7abf95ea0b0de57ec5e7435b87b37e54f3d6
vzsky <my999n@gmail.com>
2024-01-10 13:06:03 +0900

    add help

--- src/cli/main.py
+++ src/cli/main.py
@@ -1,28 +1,45 @@
 from src.cli import CliTools
+from rich.table import Table
 
 cli = CliTools()
 
-import src.cli.show
-import src.cli.fairprice
+import src.cli.fairprice   
+import src.cli.show       
 
-def main () : 
-    while True : 
-        try : 
 
-            try : 
+def help():
+    cli.cons.print("** Help - Fundalysis **", style="bold green")
+
+    command = Table(show_header=False)
+    command.add_column("Command")
+    command.add_column("Function")
+    command.add_row("help", "show this message")
+    command.add_row("show [symbol]", "show fundamental analysis of [symbol]")
+    command.add_row("fairprice (symbol)", "calculate discounted cashflow")
+    cli.cons.print(command)
+
+
+
+def main():
+    while True:
+        try:
+            try:
                 inp = input("$ ")
-                if inp == "" : continue
-                if inp == "exit" or inp == "quit" : break
-            except Exception as err: 
+                if inp == "":
+                    continue
+                if inp == "help" or inp == "h":
+                    help()
+                if inp == "exit" or inp == "quit":
+                    break
+            except Exception as err:
                 break
 
-            args = inp.split(' ')
+            args = inp.split(" ")
             command = cli.cmd(args[0])
-            if command : 
+            if command:
                 command(*args[1:])
-            else :
+            else:
                 cli.cons.print("no such command", style="bold red")
-        except Exception as e : 
+        except Exception as e:
             cli.cons.print("An error occur", style="bold red")
             cli.cons.print(e)
-        
